64d01d8463c2e547e85939910741abf91629fc5f
wenli.lw <wenli.lw@alibaba-inc.com>
2017-05-07 10:42:12 +0800

    update _config.ym

---
+++ .site/_kiss/src/blog.js
@@ -0,0 +1,139 @@
+import md from './md.js'
+import m from 'mithril'
+import './theme/hljs.css'
+import './theme/md.less'
+import './theme/style.less'
+
+const meta = (function () {
+  const _meta = {}
+  Array.prototype.forEach.call(document.querySelectorAll('meta'), function (item) {
+    if (item.name) {
+      _meta[item.name] = item.content
+    }
+  })
+  return _meta
+}())
+
+const domain = window.location.hostname === '127.0.0.1' ? './.site' : ~meta.base.indexOf('{{') ? './data' : meta.base
+
+const Layout = function (content) {
+  return m('.container', [
+    Header,
+    m('main', content),
+    Footer
+  ])
+}
+
+const Header = m('header', [
+  m('nav.navigation', [
+    m('.menu', m.trust('<svg aria-hidden="true" class="octicon octicon-three-bars" height="24" version="1.1" viewBox="0 0 12 16" width="18"><path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path></svg>')),
+    m('img', {style: {height: '27px', width: '27px'}, src: meta.logo}),
+    m('input[type=checkbox].dummy#show-menu', {style: {opacity: 0}}),
+    m('label.wrap[for="show-menu"]', m('aside', [
+      m('a', { href: '/', oncreate: m.route.link }, '首页'),
+      m('a', { href: '/projects', oncreate: m.route.link }, '项目'),
+      m('a', { href: '/slides', oncreate: m.route.link }, 'Slides'),
+      m('a', { href: '/about', oncreate: m.route.link }, '关于')
+    ]))
+  ])
+])
+
+const Footer = m('footer', [
+  m('.copy-right', '© 2017 ' + meta.author),
+  m('.links', [
+    m('a', { href: meta.github }, 'Github'),
+    m('a', { href: 'mailto:' + meta.mail + '?subject=Hello world' }, 'Mail')
+  ])
+])
+
+const Post = {
+  inited: false,
+  post: {},
+  oninit: function (vnode, id) {
+    m.request(domain + '/' + (!id ? 'post/' + vnode.attrs.category + '/' + vnode.attrs.id : vnode))
+    .then(function (ret) {
+      Post.post = ret
+    })
+  },
+  view: function () {
+    return Layout([
+      m('banner', [
+        m('h3', m('.title', this.post.title))
+      ]),
+      m('.meta', [
+        m('span.date', this.post.date),
+        m('span.category', this.post.category)
+      ]),
+      m('article.markdown-body', m.trust(md(this.post.content || ''))),
+      m('nav', [
+        this.post.prev ? m('a', {href: '#!/' + this.post.prev.url, onclick: this.oninit.bind(null, this.post.prev.url)}, '上一篇:' + this.post.prev.title) : null,
+        this.post.next ? m('a', {href: '#!/' + this.post.next.url, onclick: this.oninit.bind(null, this.post.next.url)}, '下一篇:' + this.post.next.title) : null
+      ])
+    ])
+  }
+}
+
+const Posts = {
+  posts: [],
+  getData: function () {
+    m.request(domain + '/page/', {mode: 'no-cors'}).then(function (ret) {
+      Posts.posts = ret.posts
+    })
+  },
+  oninit: function () {
+    this.getData()
+  },
+  view: function (vnode) {
+    return Layout(this.posts.map(function (item) {
+      return m('.posts', {}, [
+        m('.cell', {}, [
+          m('.head', [
+            m('h3', [
+              m('a.title', {href: '/' + item.url, oncreate: m.route.link}, item.title)
+            ])
+          ]),
+          m('.meta', [
+            m('span.date', item.date),
+            m('span.category', item.category)
+          ]),
+          m('.text.markdown-body', m.trust(md(item.summary)))
+        ])
+      ])
+    }))
+  }
+}
+
+m.route(document.body, '/', {
+  '/': Posts,
+  '/post/:category/:id': Post,
+  '/about': {
+    about: null,
+    oninit () {
+      const self = this
+      m.request({
+        url: domain + '/about.md',
+        deserialize: ret => ret
+      })
+      .then(function (ret) {
+        self.about = md(ret)
+      })
+    },
+    view () {
+      return Layout(
+        m('.about.markdown-body', m.trust(this.about))
+      )
+    }
+  },
+  '/projects': {
+    oninit () {
+      window.location.href = 'https://github.com/lwdgit?utf8=%E2%9C%93&tab=repositories&q=&type=source&language='
+    },
+    view () {}
+  },
+  '/slides': {
+    oninit () {},
+    view () {
+      return Layout('This is undefined')
+    }
+  }
+})
---
+++ .site/_kiss/src/md.js
@@ -0,0 +1,38 @@
+import MarkdownIt from 'markdown-it'
+import hljs from 'highlight.js'
+import MarkdownItLists from 'markdown-it-task-lists'
+// import './hljs.css'
+
+const md = MarkdownIt({
+  html: true, // Enable HTML tags in source
+  xhtmlOut: true, // Use '/' to close single tags (<br />).
+  // This is only for full CommonMark compatibility.
+  breaks: true, // Convert '\n' in paragraphs into <br>
+  langPrefix: 'language-', // CSS language prefix for fenced blocks. Can be
+  // useful for external highlighters.
+  linkify: true, // Autoconvert URL-like text to links
+
+  // Enable some language-neutral replacement + quotes beautification
+  typographer: true,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  //
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+  // quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externaly.
+  // If result starts with <pre... internal wrapper is skipped.
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value
+      } catch (__) {}
+    }
+
+    return '' // use external default escaping
+  }
+}).use(MarkdownItLists)
+export default md.render.bind(md)
