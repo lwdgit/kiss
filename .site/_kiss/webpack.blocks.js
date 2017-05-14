bbceea0e03f2cd11bdf23763ccf2000fa98ce44b
wenli.lw <wenli.lw@alibaba-inc.com>
2017-05-14 23:53:52 +0800

    update

--- .site/_kiss/webpack.blocks.js
+++ .site/_kiss/webpack.blocks.js
@@ -1,55 +1,55 @@
 const { addPlugins, createConfig, defineConstants, env, entryPoint, setOutput, sourceMaps, webpack } = require('@webpack-blocks/webpack2')
 const babel = require('@webpack-blocks/babel6')
+const extractText = require('@webpack-blocks/extract-text2')
 const devServer = require('@webpack-blocks/dev-server2')
 const postcss = require('@webpack-blocks/postcss')
+const { rm } = require('shelljs')
+rm('-rf', './build')
+
 const less = function (options) {
-    "use strict";
-    options = options || {};
+  'use strict'
+  options = options || {}
 
-    const hasOptions = Object.keys(options).length > 0;
+  const hasOptions = Object.keys(options).length > 0
 
-    return (context) => ({
-        module: {
-            loaders: [
-                {
-                    test: context.fileType('text/x-less'),
-                    loaders: [
-                        'style-loader',
-                        options.sourceMap ? 'css-loader?sourceMap' : 'css-loader',
-                        hasOptions ? 'less-loader?' + JSON.stringify(options) : 'less-loader'
-                    ]
-                }
-            ]
+  return (context) => ({
+    module: {
+      loaders: [
+        {
+          test: context.fileType('text/x-less'),
+          loaders: [
+            'style-loader',
+            options.sourceMap ? 'css-loader?sourceMap' : 'css-loader',
+            hasOptions ? 'less-loader?' + JSON.stringify(options) : 'less-loader'
+          ]
         }
-    });
+      ]
+    }
+  })
 }
 
 const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const httpd = require('./httpd')
-
-if (process.env['NODE_ENV'] === 'dev') {
+const isDev = process.env['NODE_ENV'] === 'dev'
+if (isDev) {
   httpd.run('8778', '../')
 }
 
 module.exports = createConfig([
   entryPoint('./src/blog.js'),
-  setOutput('./build/[name].[hash:8].js'),
+  setOutput({
+    filename: './build/[name].[hash:8].js',
+    publicPath: '/_kiss/'
+  }),
   babel(),
-  less(),
-  function () {
-    return {
-      module: {
-        loaders: [{
-          test: /\.png$/,
-          loader: 'file-loader'
-        }]
-      }
-    }
-  },
+  less({
+    relativeUrls: true
+  }),
+  // extractText('./build/css/[name].[hash:8].css', 'text/x-less'),
   addPlugins([new HtmlWebpackPlugin({
     inject: true,
-    filename: '../../app.html',
+    filename: isDev ? 'index.html' : '../app.html',
     template: './src/template.html'
   })]),
   postcss([
